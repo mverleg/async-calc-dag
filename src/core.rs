fad0d7b1508dcefdd306a754aacb3149f1fb599e
Mark <mverleg.noreply@gmail.com>
2025-03-21 21:41:08 -0500

    Only read once from MockFs

--- src/core.rs
+++ src/core.rs
@@ -1,5 +1,13 @@
+use crate::ast::Ast;
+use crate::common::Error;
+use crate::file::File;
+use crate::lazy_async::ALazy;
 
-pub struct Core {}
+pub struct Core {
+    files: Vec<ALazy<File>>,
+    asts: Vec<ALazy<Ast>>,
+    output: Vec<ALazy<Result<i64, Error>>>,
+}
 
 impl Core {
     
