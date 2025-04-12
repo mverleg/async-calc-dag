309eea830f78cca2cc022fc2cb9152260eafda6a
Mark <mverleg.noreply@gmail.com>
2025-04-12 18:59:19 -0500

    Switch from Mutex to OnceLock

--- src/lazy_async.rs
+++ src/lazy_async.rs
@@ -1,25 +1,22 @@
 use crate::common::Error;
-use ::futures::lock::Mutex as AsyncMutex;
-use ::futures::lock::MutexGuard;
 use ::std::sync::Arc;
-
 // TODO @mverleg: how to make somewhat sure there is no deadlock if the creator of ALazy doesn't put a value in?
 
 // TODO: this could probably be more efficient by using the fact that we can only go from empty to full and never back, e.g. double checked atomic
 pub struct ALazy<T> {
-    value: Arc<AsyncMutex<Option<Result<T, Error>>>>,
+    value: Arc<OnceLock<Result<T, Error>>>,
 }
 
 impl <T> ALazy<T> {
     // It seems nice to pass the initializer into the factory method, since that nicely
     // models that it should happen no more and no less than one time. However, we should
     // put the ALazy into a container before it's finished constructing.
-    pub async fn new_empty() -> Result<Self, Error> {
-        ALazy { value: Arc::new(AsyncMutex::new(None)) }.await
+    pub async fn new_empty() -> Self {
+        ALazy { value: Arc::new(OnceLock::new()) }
     }
 
     pub async fn get(&self) -> &Result<T, Error> {
-        self.value.lock().await
+        self.value.get().await
     }
 
     // TODO for now no async `f` arg, to avoid deadlocks
